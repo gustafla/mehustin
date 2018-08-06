96d8fe3878dfe96a6feba5b8c5abc44f6c8fa756
Lauri Gustafsson <luutifa@gmail.com>
2018-08-06 11:48:32 +0300

    Use gmake feature for ignoring errors

--- executable.mk
+++ executable.mk
@@ -13,8 +13,8 @@ include common.mk
 .PHONY: install
 
 install: $(TARGET) LICENSE demo.sh
-	mkdir -p $(PREFIX)/bin || :
-	mkdir -p $(PREFIX)/src || :
+	-mkdir -p $(PREFIX)/bin
+	-mkdir -p $(PREFIX)/src
 	cp $(TARGET) $(PREFIX)/bin/
 	cp LICENSE $(PREFIX)/
 	cp demo.sh $(PREFIX)/
--- module.mk
+++ module.mk
@@ -16,10 +16,10 @@ include common.mk
 # expects make -f executable.mk install to be done first for the PREFIX
 install: $(TARGET)
 	cp $(TARGET) $(PREFIX)/bin/
-	cp *.vert $(PREFIX)/bin/ || :
-	cp *.frag $(PREFIX)/bin/ || :
-	cp *.track $(PREFIX)/bin/ || :
-	cp music.ogg $(PREFIX)/bin/ || :
-	cp README.md $(PREFIX)/ || :
+	-cp *.vert $(PREFIX)/bin/
+	-cp *.frag $(PREFIX)/bin/
+	-cp *.track $(PREFIX)/bin/
+	-cp music.ogg $(PREFIX)/bin/
+	-cp README.md $(PREFIX)/
 	cp *.c $(PREFIX)/src/
 	cp *.h $(PREFIX)/src/
