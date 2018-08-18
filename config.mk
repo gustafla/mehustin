d7e3de6349bf83aa1e2342dab2407661ae0e442c
Lauri Gustafsson <luutifa@gmail.com>
2018-08-18 12:14:27 +0300

    Removed NIH math code and integrated recp/cglm

--- config.mk
+++ config.mk
@@ -4,7 +4,7 @@ BASE?=.
 CC=gcc
 PKGS=sdl2 gl
 PKGS_MODULE=gl
-CFLAGS+=$(shell pkg-config --cflags $(PKGS)) -std=c99 -I$(BASE) -I$(BASE)/lib/stb -I$(BASE)/lib/rocket/lib -DGL_GLEXT_PROTOTYPES
+CFLAGS+=$(shell pkg-config --cflags $(PKGS)) -std=c99 -I$(BASE) -I$(BASE)/lib/stb -I$(BASE)/lib/rocket/lib -I$(BASE)/lib/cglm/include -DGL_GLEXT_PROTOTYPES
 
 # debug and release settings
 release:CFLAGS+=-O2 -ffast-math -s -DSYNC_PLAYER
