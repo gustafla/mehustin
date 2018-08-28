b3e88667c6ca3c0d8418e12dab8ad99195e7adeb
Lauri Gustafsson <luutifa@gmail.com>
2018-08-28 22:17:08 +0300

    Fix procedural_texture init order

--- procedural_texture.c
+++ procedural_texture.c
@@ -8,10 +8,6 @@ static mat4 identity = GLM_MAT4_IDENTITY_INIT;
 
 GLuint texture_render(GLsizei w, GLsizei h, char *fragment_path,
         tex_image_2d_t format) {
-    GLint viewport[4];
-    glGetIntegerv(GL_VIEWPORT, viewport);
-    glViewport(0, 0, w, h);
-
     const tex_image_2d_t *formats[] = {&format, NULL};
     fbo_t *fbo = fbo_init(w, h, formats);
     if (!fbo) return 0;
@@ -21,13 +17,18 @@ GLuint texture_render(GLsizei w, GLsizei h, char *fragment_path,
         fbo_free(fbo);
         return 0;
     }
-    ufm_mat4(program, "model", identity);
-    ufm_mat4(program, "view", identity);
-    ufm_mat4(program, "projection", identity);
+
+    GLint viewport[4];
+    glGetIntegerv(GL_VIEWPORT, viewport);
+    glViewport(0, 0, w, h);
 
     fbo_bind(fbo);
     glUseProgram(program);
+    ufm_mat4(program, "model", identity);
+    ufm_mat4(program, "view", identity);
+    ufm_mat4(program, "projection", identity);
     quad_render();
+
     glDeleteProgram(program);
     glViewport(viewport[0], viewport[1], viewport[2], viewport[3]);
 
