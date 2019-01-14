b2ba12e40907062d9c9e594ab28f7b042db258f5
Lauri Gustafsson <luutifa@jkry.org>
2019-01-14 23:10:36 +0200

    Add automatic mkdir commands to makefile

--- common.mk
+++ common.mk
@@ -2,10 +2,12 @@ OBJS=$(patsubst %.c,%.o,$(SOURCES:src/%=$(BUILDDIR)/%))
 
 # link target
 $(TARGET): $(OBJS)
+	@mkdir -p $(@D)
 	$(CC) -o $(TARGET) $(CFLAGS) $(OBJS) $(LFLAGS) $(LDLIBS)
 
 # compile target
 $(BUILDDIR)/%.o: $(SOURCES)
+	@mkdir -p $(@D)
 	$(CC) $(CFLAGS) -c -o $@ $<
 
 .PHONY: clean
